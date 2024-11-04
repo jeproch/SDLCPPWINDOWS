05db93f8de698e3ae64a36b5c6a94ac5aae7d0f4
jeproch <jprevenant@gmail.com>
2024-11-04 19:10:19 +0200

    removed button

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 
 // Define the button's rectangle
-SDL_Rect buttonRect = {500, 250, 200, 100}; // Position and size of the button
 
 int InitializeSDL() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -38,35 +37,17 @@ void CleanUp() {
     SDL_Quit();
 }
 
-bool IsMouseInsideButton(int mouseX, int mouseY) {
-    return mouseX > buttonRect.x && mouseX < buttonRect.x + buttonRect.w &&
-           mouseY > buttonRect.y && mouseY < buttonRect.y + buttonRect.h;
-}
-
 int main(int argc, char* argv[]) {
     if (InitializeSDL() != 0) return 1;
 
     bool isRunning = true;
     SDL_Event event;
-    bool buttonHovered = false;
     
     while (isRunning) {
         // Poll events
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 isRunning = false;
-            }
-            else if (event.type == SDL_MOUSEMOTION || event.type == SDL_MOUSEBUTTONDOWN) {
-                int mouseX, mouseY;
-                SDL_GetMouseState(&mouseX, &mouseY);
-
-                // Check if the mouse is inside the button area
-                buttonHovered = IsMouseInsideButton(mouseX, mouseY);
-
-                // Handle mouse click on the button
-                if (buttonHovered && event.type == SDL_MOUSEBUTTONDOWN) {
-                    std::cout << "Button clicked!" << std::endl;
-                }
             }
         }
 
@@ -75,12 +56,6 @@ int main(int argc, char* argv[]) {
         SDL_RenderClear(renderer);
 
         // Draw the button with different color if hovered
-        if (buttonHovered) {
-            SDL_SetRenderDrawColor(renderer, 200, 0, 0, 255); // Red for hover
-        } else {
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White
-        }
-        SDL_RenderFillRect(renderer, &buttonRect);
 
         // Present the rendered content
         SDL_RenderPresent(renderer);
