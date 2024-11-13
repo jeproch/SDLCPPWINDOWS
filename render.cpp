b3858cc5eaa76b224ccf35707439cd658eed3511
jeproch <jprevenant@gmail.com>
2024-11-13 17:23:29 +0200

    Code Refactor

--- render.cpp
+++ render.cpp
@@ -10,60 +10,85 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 
 int Render::InitializeSDL() {
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL Initialization Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
+    // Initialize SDL_image for PNG support
     if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
         std::cerr << "IMG_Init Error: " << IMG_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
-    
+
+    // Create SDL window
     window = SDL_CreateWindow("SDL Image Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 600, SDL_WINDOW_SHOWN);
     if (!window) {
         std::cerr << "SDL Window Creation Error: " << SDL_GetError() << std::endl;
+        IMG_Quit();
         SDL_Quit();
         return 1;
     }
-    
+
+    // Create SDL renderer
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         std::cerr << "SDL Renderer Creation Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
+        IMG_Quit();
         SDL_Quit();
         return 1;
     }
-    
-    return 0;
+
+    return 0; // success
 }
 
 bool isRunning;
 bool showImage1 = true;
+int textureWidth, textureHeight;
+int toggleInterval;
+//SDL
 SDL_Event event;
+SDL_Rect dstRect;
+
+void CheckTextures() {
+    if (!imageTexture1 || !imageTexture2) {
+        std::cerr << "IMG_LoadTexture Error: " << IMG_GetError() << std::endl;
+        return;
+    }
+}
+
+void Render::SetBackgroundColour(int r, int g, int b, int a) {
+    SDL_SetRenderDrawColor(renderer, r, g, b, a);
+    // SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
+    SDL_RenderClear(renderer);
+}
+
+void Render::FreeTextures() {
+    SDL_DestroyTexture(imageTexture1);
+    SDL_DestroyTexture(imageTexture2);
+}
 
 void Render::RenderLoop() {
     isRunning = true;
 
     // Load the images as textures
-    SDL_Texture* imageTexture1 = IMG_LoadTexture(renderer, "src/png-transparent-spider-man-heroes-download-with-transparent-background-free-thumbnail.png");
-    SDL_Texture* imageTexture2 = IMG_LoadTexture(renderer, "src/images.jpg");
+    imageTexture1 = IMG_LoadTexture(renderer, "src/png-transparent-spider-man-heroes-download-with-transparent-background-free-thumbnail.png");
+    imageTexture2 = IMG_LoadTexture(renderer, "src/images.jpg");
 
-    if (!imageTexture1 || !imageTexture2) {
-        std::cerr << "IMG_LoadTexture Error: " << IMG_GetError() << std::endl;
-        return;
-    }
+    CheckTextures();
 
     // Get the width and height of the textures
-    int textureWidth, textureHeight;
+    
     SDL_QueryTexture(imageTexture1, NULL, NULL, &textureWidth, &textureHeight);
 
-    SDL_Rect dstRect = {100, 100, textureWidth, textureHeight};
+    dstRect = {100, 100, textureWidth, textureHeight};
 
     // Variables to control texture switching
     auto lastToggleTime = std::chrono::high_resolution_clock::now();
-    int toggleInterval = 500; // milliseconds
+    toggleInterval = 500; // milliseconds
 
     // Render loop
     while (isRunning) {
@@ -85,8 +110,7 @@ void Render::RenderLoop() {
         }
 
         // Set background color
-        SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
-        SDL_RenderClear(renderer);
+        Render::SetBackgroundColour(0,128,128,255);            // SDL_SetRenderDrawColor(renderer, 0, 128, 128, 255);
 
         // Render the appropriate texture based on `showImage1`
         if (showImage1) {
@@ -100,8 +124,7 @@ void Render::RenderLoop() {
     }
 
     // Free textures once we're done
-    SDL_DestroyTexture(imageTexture1);
-    SDL_DestroyTexture(imageTexture2);
+    Render::FreeTextures();
 }
 
 void Render::CleanUp() {
--- render.h
+++ render.h
@@ -8,11 +8,15 @@ extern  SDL_Window* window;
 extern  SDL_Renderer* renderer;
 extern bool isRunning;
 extern SDL_Event event;
+extern SDL_Texture* imageTexture1;
+extern SDL_Texture* imageTexture2;
 
 namespace Render {
     int InitializeSDL();
     void CleanUp();
     void RenderLoop();
+    void SetBackgroundColour(int r, int g, int b, int a);
+    void FreeTextures();
 }
 
 #endif
