f001052563c78c44ee338acd339b69d37c118d9c
jeproch <jprevenant@gmail.com>
2024-11-17 20:33:29 +0200

    texture reference

--- src-cpp/render.h
+++ src-cpp/render.h
@@ -8,6 +8,17 @@
 extern  SDL_Window* window;
 extern  SDL_Renderer* renderer;
 extern bool isRunning;
+extern bool showImage1;
+
+extern SDL_Texture* imageTexture1;
+extern SDL_Texture* imageTexture2;
+extern SDL_Texture* imageTextureA;
+extern SDL_Texture* imageTextureD;
+extern SDL_Texture* imageTextureS;
+extern SDL_Texture* imageTextureW;
+
+
+extern SDL_Rect dstRect;
 extern SDL_Event event;
 
 namespace Render {
@@ -17,6 +28,10 @@ namespace Render {
     void SetBackgroundColour(int r, int g, int b, int a);
     void FreeTextures();
     void DeterminePlayerTexture();
+}
+
+namespace Keyboard {
+    void ReadKeyboardInput();
 }
 
 #endif
