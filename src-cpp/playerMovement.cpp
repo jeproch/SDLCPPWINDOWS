efe4006312f436f8a5631e7c8cee51dbd869d1de
jeproch <jprevenant@gmail.com>
2024-12-13 19:23:03 +0200

    Fix: out of bounds bottom and right of border

--- src-cpp/playerMovement.cpp
+++ src-cpp/playerMovement.cpp
@@ -11,48 +11,42 @@ void Movement::PlayerMove() {
     auto now = std::chrono::steady_clock::now();
     if (now - lastUpdate >= updateInterval) {
         // Enough time has passed, update movement
-        //collision detection
-        switch (playerState)
-        {
+        // Collision detection
+        switch (playerState) {
             case PlayerState::MovingW:
-
+                // Check if the player can move upward
                 if (map[(dstRect.y / TILE_SIZE) - 1][dstRect.x / TILE_SIZE] != 1) {
                     dstRect.y -= TILE_SIZE;
                 }
-                         
                 break;
-            case PlayerState::MovingA:
 
+            case PlayerState::MovingA:
+                // Check if the player can move left
                 if (map[(dstRect.y / TILE_SIZE)][(dstRect.x / TILE_SIZE) - 1] != 1) {
                     dstRect.x -= TILE_SIZE;
                 }
-                
-            
-            
                 break;
+
             case PlayerState::MovingS:
-
-
-                if (map[(dstRect.y / TILE_SIZE) + 1][dstRect.x / TILE_SIZE] != 1) {
+                // Check if the player can move downward (boundary check for the last row)
+                if ((dstRect.y / TILE_SIZE) + 1 < MAP_HEIGHT &&
+                    map[(dstRect.y / TILE_SIZE) + 1][dstRect.x / TILE_SIZE] != 1) {
                     dstRect.y += TILE_SIZE;
                 }
-            
-            
                 break;
+
             case PlayerState::MovingD:
-                
-                
-                if (map[(dstRect.y / TILE_SIZE)][(dstRect.x / TILE_SIZE) + 1] != 1) {
+                // Check if the player can move right (boundary check for the last column)
+                if ((dstRect.x / TILE_SIZE) + 1 < MAP_WIDTH &&
+                    map[(dstRect.y / TILE_SIZE)][(dstRect.x / TILE_SIZE) + 1] != 1) {
                     dstRect.x += TILE_SIZE;
                 }
-                
-                
                 break;
+
             default:
                 break;
         }
 
         lastUpdate = now; // Reset the timer
     }
-
 }
